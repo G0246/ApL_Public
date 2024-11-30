3e52e4a0e7a995d673dd0f74837b2f28b2830b24
Veutexus <70145360+G0246@users.noreply.github.com>
2024-11-30 03:05:16 +0000

    Added edit function

--- ExpenseTracker/ExpenseTracker.py
+++ ExpenseTracker/ExpenseTracker.py
@@ -39,6 +39,48 @@ def View():
         print("Total Expenses: ", total_expense)
         print("Net Total: ", net_total)
 
+def Edit():
+    if dataset.empty:
+        print("No transactions to edit.")
+        return
+
+    print("Current Transactions:")
+    print(dataset)
+
+    index = int(input("Enter the index of the transaction you want to edit (Enter -1 to cancel this operation): "))
+
+    if 0 <= index < len(dataset):
+        print("Editing Transaction at index:", index)
+        
+        new_date = input(f"Enter the new date (Current: {dataset.at[index, 'Date']}): ") or dataset.at[index, "Date"]
+        new_category = input(f"Enter the new category (Current: {dataset.at[index, "Category"]}): ") or dataset.at[index, "Category"]
+        new_amount = input(f"Enter the new amount (Current: {dataset.at[index, "Amount"]}): ")
+        new_transaction_type = input(f"Enter the new type (Current: {dataset.at[index, "Type"]})) (Income = 0 / Expense = 1): ")
+
+        if not new_amount:
+            dataset.at[index, "Amount"]
+        else:
+            new_amount = float(new_amount)
+
+        if new_transaction_type == "1":
+            new_transaction_type = "Expense"
+        elif new_transaction_type == "0":
+            new_transaction_type = "Income"
+        else:
+            new_transaction_type = dataset.at[index, "Type"]
+
+        dataset.at[index, "Date"] = new_date
+        dataset.at[index, "Category"] = new_category
+        dataset.at[index, "Amount"] = new_amount
+        dataset.at[index, "Type"] = new_transaction_type
+
+        print("Transaction updated successfully.")
+    elif index == -1:
+        print("Operation cancelled.")
+        return
+    else:
+        print("Invalid index. No transaction edited.")
+
 def Delete():
     if dataset.empty:
         print("No transactions to delete.")
@@ -59,16 +101,18 @@ def Delete():
         print("Invalid index. No transaction deleted.")
 
 while True:
-    print("\nPersonal Expense Tracker \n1. Add Transaction \n2. View Summary \n3. Delete Transaction \n4. Save & Exit")
+    print("\nPersonal Expense Tracker \n1. Add Transaction \n2. Edit Transaction \n3. Delete Transaction \n4. View Summary \n5. Save & Exit")
     choice = int(input("Enter the number of your choice: "))
     
     if choice == 1:
         Add()
     elif choice == 2:
-        View()
+        Edit()
     elif choice == 3:
         Delete()
     elif choice == 4:
+        View()
+    elif choice == 5:
         dataset.to_csv('Expenses.csv', index = False)
         print("Expenses saved to Expenses.csv. Exiting...")
         break
