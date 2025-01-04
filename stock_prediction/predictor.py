fb744800418c3e0e8aff920e8b4d095bc1fee803
Veutexus <70145360+G0246@users.noreply.github.com>
2025-01-04 04:17:07 +0000

    stock initial

---
+++ stock_prediction/predictor.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt, requests
+
+url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo'
+r = requests.get(url)
+data = r.json()
+
+print(data)
