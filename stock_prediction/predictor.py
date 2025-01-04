15a106d12163a0e22f9bfddba4a9b329b26f70a3
Veutexus <70145360+G0246@users.noreply.github.com>
2025-01-04 04:30:43 +0000

    initial

--- stock_prediction/predictor.py
+++ stock_prediction/predictor.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt, requests
 
-url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo'
+url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo"
 r = requests.get(url)
 data = r.json()
-
 print(data)
