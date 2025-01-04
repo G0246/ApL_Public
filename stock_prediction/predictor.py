ac98133e48c3b8713224d36a7dc16214f0fab0c1
Veutexus <70145360+G0246@users.noreply.github.com>
2025-01-04 05:02:33 +0000

    v2

--- stock_prediction/predictor.py
+++ stock_prediction/predictor.py
@@ -1,7 +1,10 @@
 import matplotlib.pyplot as plt, requests
 
-# API
-url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=EL1WRWLWB9B05AS1"
+# Linear regression
+# https://scikit-learn.org/stable/user_guide.html
+# API https://www.alphavantage.co/documentation/
+
+url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=AAPL&interval=5min&apikey=EL1WRWLWB9B05AS1"
 r = requests.get(url)
 data = r.json()
 print(data)
