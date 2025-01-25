1923f4f118770cea2b0129ef241734a0b9f71f7e
Veutexus <70145360+G0246@users.noreply.github.com>
2025-01-25 07:10:57 +0000

    feat: pygame

--- shooting_game/config.py
+++ shooting_game/config.py
@@ -1,16 +1,16 @@
-# An actual configuration
-
-BG = ""
-
 # Player
-PLAYER_FIRE_RATE = 5
+PLAYER_FIRE_COOLDOWN = 500  # In milliseconds
 PLAYER = (155, 590, 50, 50)
 PLAYER_SPEED = 10
+
+# Bullets
+BULLET_WIDTH = 5
+BULLET_HEIGHT = 10
 
 # Display
 DISPLAY_SIZE = (360, 640)
 
 # Enemy
-ENEMY_SPAWN_RATE = 10
-ENEMY_SPEED  = 5
-ENEMY = (155, 0, 50, 50)
+ENEMY_SPAWN_RATE = 30  # Higher value = less frequent spawns
+ENEMY_SPEED = 5
+ENEMY_RADIUS = 15
